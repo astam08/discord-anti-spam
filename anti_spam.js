748634a868354896ef2dd1407efed63affa536ca
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-24 14:18:29 +0700

    Update anti_spam.js

--- anti_spam.js
+++ anti_spam.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js"); // Requiring Library just for the sick of being there
 const antispam = require ("discord-anti-spam");
+const client = new Discord.Client();
 
 var authors = [];
 var warned = [];
